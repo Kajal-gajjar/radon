633e2c98a0c6fea3d672fdcbd21f7f136e3f5d1f
Kajal-gajjar <kngajjar123@gmail.com>
2022-06-02 12:00:46 +0400

    query-params

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("../logger/logger");
 const helper = require("../util/helper");
 const validator = require("../validator/formatter");
+const chunk = require("chunk");
 
 const router = express.Router();
 
@@ -14,6 +15,38 @@ router.get("/test-me", function (req, res) {
   validator.changetoLowerCase();
   validator.changeToUpperCase();
   res.send("Assignment- Node Modules");
+});
+
+router.get("/hello", function (req, res) {
+  let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let result = chunk(month, 4);
+  console.log("The sub-array of Months: ", result);
+});
+
+router.get("/candidate", function (req, res) {
+  console.log("Query Object: ", JSON.stringify(req.query));
+  const gender = req.query.gender;
+  console.log(gender);
+  let candidate = ["Kajal", "Ankita"];
+  res.send(candidate);
+});
+
+router.get("/candidate/:name", function (req, res) {
+  console.log(req.params.name);
+  res.send("Done");
 });
 
 module.exports = router;
