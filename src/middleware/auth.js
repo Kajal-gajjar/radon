391bc272d946a8dcd89c055811892826cbc7ea06
Kajal-gajjar <kngajjar123@gmail.com>
2022-06-15 08:30:12 +0400

    assignment/auth-1

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,9 +6,12 @@ const userVerification = function (req, res, next) {
 
   //If no token is present in the request header return error
   if (!token) return res.send({ status: false, msg: "token must be present" });
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
+  try {
+    let decodedToken = jwt.verify(token, "functionup-radon");
+  } catch (err) {
     return res.send({ status: false, msg: "token is invalid" });
+  }
+
   next();
 };
 
